8fb7a7c67acfb38232c4e5793377e0e13c635423
m4tt075 <mgawalter@web.de>
2019-02-04 21:16:11 +0100

    [SynoCLI-file] Integrate and upgrade mc to 4.8.22

--- spk/synocli-file/src/service-setup.sh
+++ spk/synocli-file/src/service-setup.sh
@@ -10,6 +10,12 @@ service_postinst ()
         fi
     done
 
+    # Set custom links for midnight commander
+    ln -s ${SYNOPKG_PKGDEST}/bin/mc-utf8 /usr/local/bin/mc
+    ln -s ${SYNOPKG_PKGDEST}/bin/mc-utf8 /usr/local/bin/mcedit
+    ln -s ${SYNOPKG_PKGDEST}/bin/mc-utf8 /usr/local/bin/mcdiff
+    ln -s ${SYNOPKG_PKGDEST}/bin/mc-utf8 /usr/local/bin/mcview
+
     # rename requires perl modules to be linked into local installation
     ln -s "${SYNOPKG_PKGDEST}/share/perl5/File/Rename.pm" "/usr/local/share/perl5/vendor_perl/File/Rename.pm"
     mkdir "/usr/local/share/perl5/vendor_perl/File/Rename"
@@ -24,6 +30,13 @@ service_postuninst ()
             rm -f "/usr/local/bin/$cmd"
         fi
     done
+
+    # Remove custom links for midnight commander
+    rm -f /usr/local/bin/mc
+    rm -f /usr/local/bin/mcedit
+    rm -f /usr/local/bin/mcdiff
+    rm -f /usr/local/bin/mcview
+
     # Remove rename's perl modules from the local installation
     if [ -e "/usr/local/share/perl5/vendor_perl/File/Rename.pm" ]; then
         rm -f "/usr/local/share/perl5/vendor_perl/File/Rename.pm"
